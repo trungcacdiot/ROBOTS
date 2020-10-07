b0cbdec513d66ed6a2e0edf5f44097454a22987e
trungcacdiot <68458523+trungcacdiot@users.noreply.github.com>
2020-10-07 13:25:18 +0700

    Updates

---
+++ precache-manifest.5f65102bfc60c1415c6dda1414673ec4.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3d9200949231993549b8b7c3740b62",
+    "url": "/ROBOTS/index.html"
+  },
+  {
+    "revision": "523d9c553429631e2a16",
+    "url": "/ROBOTS/static/css/main.0121ea8f.chunk.css"
+  },
+  {
+    "revision": "31ab3c42ae9fe1ab5756",
+    "url": "/ROBOTS/static/js/2.4b847e85.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ROBOTS/static/js/2.4b847e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523d9c553429631e2a16",
+    "url": "/ROBOTS/static/js/main.e514ce08.chunk.js"
+  },
+  {
+    "revision": "632f623e51c39c9d54a2",
+    "url": "/ROBOTS/static/js/runtime-main.490e60db.js"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/ROBOTS/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "43dd5b7a3d277362d5e801e5353e3a01",
+    "url": "/ROBOTS/static/media/Montserrat-Thin.43dd5b7a.ttf"
+  },
+  {
+    "revision": "dc880f17456d5cad7f07d83554c22b9b",
+    "url": "/ROBOTS/static/media/chevron-left-solid.dc880f17.svg"
+  },
+  {
+    "revision": "1fce70541cd225507e29c233e23f2cb2",
+    "url": "/ROBOTS/static/media/chevron-right-solid.1fce7054.svg"
+  },
+  {
+    "revision": "3b30c82204fe750dcaa3c5113585f6ee",
+    "url": "/ROBOTS/static/media/chevron-up-solid.3b30c822.svg"
+  },
+  {
+    "revision": "62d8b6ef8e6c796a5ddad822dff96874",
+    "url": "/ROBOTS/static/media/facebook-brands.62d8b6ef.svg"
+  },
+  {
+    "revision": "276f52a709627c3d36a273e940fc93b2",
+    "url": "/ROBOTS/static/media/google-plus-g-brands.276f52a7.svg"
+  },
+  {
+    "revision": "311b7fc943f6469bbe77fe30e939cfc2",
+    "url": "/ROBOTS/static/media/heart-regular.311b7fc9.svg"
+  },
+  {
+    "revision": "639cc557cb298870daf69fbeaaa6e017",
+    "url": "/ROBOTS/static/media/picture-1.639cc557.jpg"
+  },
+  {
+    "revision": "b916684b11239c16911ebe0bd892a732",
+    "url": "/ROBOTS/static/media/picture-10.b916684b.jpg"
+  },
+  {
+    "revision": "83e4d826c930ff3cedfbd4296b0f9701",
+    "url": "/ROBOTS/static/media/picture-2.83e4d826.jpg"
+  },
+  {
+    "revision": "d5378bab6cda9a910bcf34ac178ef1f6",
+    "url": "/ROBOTS/static/media/picture-3.d5378bab.jpg"
+  },
+  {
+    "revision": "58ab4d69b9f45c9917fbba8ee6f977b0",
+    "url": "/ROBOTS/static/media/picture-3jpg.58ab4d69.jpg"
+  },
+  {
+    "revision": "58bb29dbbc3b99b95e0286021bf5e635",
+    "url": "/ROBOTS/static/media/picture-4.58bb29db.jpg"
+  },
+  {
+    "revision": "f9f04f7d1bcf493158273da41b457682",
+    "url": "/ROBOTS/static/media/picture-5.f9f04f7d.jpg"
+  },
+  {
+    "revision": "869ee25c5c38c7703662ce5fae69b672",
+    "url": "/ROBOTS/static/media/picture-6.869ee25c.jpg"
+  },
+  {
+    "revision": "a5a2a129abff844323f49fba45b6b2f0",
+    "url": "/ROBOTS/static/media/picture-7.a5a2a129.jpg"
+  },
+  {
+    "revision": "27e381ef762bab659436c319ee32f1c7",
+    "url": "/ROBOTS/static/media/picture-8.27e381ef.jpg"
+  },
+  {
+    "revision": "5616369fd186082488263372efdcb3ea",
+    "url": "/ROBOTS/static/media/picture-9.5616369f.jpg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/ROBOTS/static/media/search-solid.c6a0effb.svg"
+  },
+  {
+    "revision": "29def3e5dd923346cc9efa53b9bc0b65",
+    "url": "/ROBOTS/static/media/user-regular.29def3e5.svg"
+  }
+]);
